da6d8b0762e23e50fa01cb8f65cc44c0732d7852
Eka Juliantara <eka_juliantara@Ekas-MacBook-Pro.local>
2025-11-26 11:35:43 +0700

    Set up the state management

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,3 +1,45 @@
 <template>
   <h3>To-Do ⏳</h3>
+  <form @submit.prevent="storeTodo(todo)">
+    <input v-model="todo.text" type="text" name="text" />
+    <button :disabled="!todo.text" type="submit" style="margin: 0 8px;">Add</button>
+  </form>
+  <div>
+    <ul>
+      <li v-for="pendingTodo in pendingTodos" :key="pendingTodo.id" style="margin: 8px 0">
+        <span style="margin: 0 8px;">{{ pendingTodo.text }}</span>
+        <button @click="updateTodo({ ...pendingTodo, isCompleted: true})" style="margin: 0 4px;">✅</button>
+        <button @click="destroyTodo(pendingTodo.id)">❌</button>
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapActions, mapState } from 'pinia';
+import { useTodos } from '@/stores/todos';
+
+export default {
+  data: () => ({
+    todo: {
+      id: null,
+      text: null,
+      isCompleted: false
+    }
+  }),
+  computed: {
+    ...mapState(useTodos, [
+      'todos', 
+      'pendingTodos', 
+      'completedTodos'
+    ])
+  },
+  methods: {
+    ...mapActions(useTodos, [
+      'storeTodo', 
+      'updateTodo',
+      'destroyTodo'
+    ]),
+  }
+}
+</script>
